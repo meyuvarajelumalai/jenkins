46645ae8a3019a54d6e6285570e1a5aa2d2438b6
meyuvarajelumalai <yuvaarajme@gmail.com>
2023-06-02 18:01:44 +0530

    updated

---
+++ Reports.html/report.js
@@ -0,0 +1,430 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("D:/C drive/Yui/eclipse-workspace/cucumber/src/test/java/com/feature/Adactin.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Checking the hotel booking functionality of an Adactin application",
+  "description": "",
+  "id": "checking-the-hotel-booking-functionality-of-an-adactin-application",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 2,
+  "name": "Login Functionality",
+  "description": "",
+  "id": "checking-the-hotel-booking-functionality-of-an-adactin-application;login-functionality",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 3,
+  "name": "user Must Launch The Url",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 4,
+  "name": "user Enter The Username In The Username Field",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 5,
+  "name": "user Enter The Password In The Password Field",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 6,
+  "name": "user Click The Login Button And It Navigates the To the Serch Hotel Page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Stepdefinition1.user_Must_Launch_The_Url()"
+});
+formatter.result({
+  "duration": 4985966100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Stepdefinition1.user_Enter_The_Username_In_The_Username_Field()"
+});
+formatter.result({
+  "duration": 165772500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Stepdefinition1.user_Enter_The_Password_In_The_Password_Field()"
+});
+formatter.result({
+  "duration": 60571600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Stepdefinition1.user_Click_The_Login_Button_And_It_Navigates_the_To_the_Serch_Hotel_Page()"
+});
+formatter.result({
+  "duration": 3000792500,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 7,
+  "name": "Search Hotel Functionality",
+  "description": "",
+  "id": "checking-the-hotel-booking-functionality-of-an-adactin-application;search-hotel-functionality",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 8,
+  "name": "user Must Select the Options",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 9,
+  "name": "user Select Location In The Select Location Field",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 10,
+  "name": "user Select Hotels In The Select Hotels Field",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 11,
+  "name": "user Select Room Type In The Select Room Type Field",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 12,
+  "name": "user Select Room Nos In The Select Room Nos Field",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 13,
+  "name": "user Enter The Datepick_In And Clear  Datepick_In Field",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 14,
+  "name": "user Enter The Datepick_In In And Pass SendKeys Datepick_In Field",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 15,
+  "name": "user Enter The Datepick_Out And Clear Datepick_Out Field",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 16,
+  "name": "user Enter The Datepick_Out And Pass SendKeys Datepick_Out Field",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 17,
+  "name": "user Enter The Adult_Room In The Adult_Room Field",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 18,
+  "name": "user Enter The Child_Room In The Child_Room Field",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 19,
+  "name": "user Click The Submit Button",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Stepdefinition1.user_Must_Select_the_Options()"
+});
+formatter.result({
+  "duration": 116600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Stepdefinition1.user_Select_Location_In_The_Select_Location_Field()"
+});
+formatter.result({
+  "duration": 167584400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Stepdefinition1.user_Select_Hotels_In_The_Select_Hotels_Field()"
+});
+formatter.result({
+  "duration": 148994400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Stepdefinition1.user_Select_Room_Type_In_The_Select_Room_Type_Field()"
+});
+formatter.result({
+  "duration": 102743100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Stepdefinition1.user_Select_Room_Nos_In_The_Select_Room_Nos_Field()"
+});
+formatter.result({
+  "duration": 127066300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Stepdefinition1.user_Enter_The_Datepick_In_And_Clear_Datepick_In_Field()"
+});
+formatter.result({
+  "duration": 71371700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Stepdefinition1.user_Enter_The_Datepick_In_In_And_Pass_SendKeys_Datepick_In_Field()"
+});
+formatter.result({
+  "duration": 102277200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Stepdefinition1.user_Enter_The_Datepick_Out_And_Clear_Datepick_Out_Field()"
+});
+formatter.result({
+  "duration": 81113200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Stepdefinition1.user_Enter_The_Datepick_Out_And_Pass_SendKeys_Datepick_Out_Field()"
+});
+formatter.result({
+  "duration": 137890300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Stepdefinition1.user_Enter_The_Adult_Room_In_The_Adult_Room_Field()"
+});
+formatter.result({
+  "duration": 123246000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Stepdefinition1.user_Enter_The_Child_Room_In_The_Child_Room_Field()"
+});
+formatter.result({
+  "duration": 103505700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Stepdefinition1.user_Click_The_Submit_Button()"
+});
+formatter.result({
+  "duration": 749431900,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 20,
+  "name": "Select Page Functionality",
+  "description": "",
+  "id": "checking-the-hotel-booking-functionality-of-an-adactin-application;select-page-functionality",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 21,
+  "name": "user Must Select the Page Options",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 22,
+  "name": "user Click The Radio Button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 23,
+  "name": "user Click The Continue Button And It Navigates To The Book Hotel Page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Stepdefinition1.user_Must_Select_the_Page_Options()"
+});
+formatter.result({
+  "duration": 51800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Stepdefinition1.user_Click_The_Radio_Button()"
+});
+formatter.result({
+  "duration": 103604700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Stepdefinition1.user_Click_The_Continue_Button_And_It_Navigates_To_The_Book_Hotel_Page()"
+});
+formatter.result({
+  "duration": 696530300,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 24,
+  "name": "Book Page Functionality",
+  "description": "",
+  "id": "checking-the-hotel-booking-functionality-of-an-adactin-application;book-page-functionality",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 25,
+  "name": "Enter the Options",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 26,
+  "name": "user Enter The Fname In The Fname Field",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 27,
+  "name": "user Enter The Lname In The Lname Field",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 28,
+  "name": "user Enter The Addres In The Addres Field",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 29,
+  "name": "user Enter The Ccno In The Ccno Field",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 30,
+  "name": "user Select Cct In The Cct Field",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 31,
+  "name": "user Select Em In The Em Field",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 32,
+  "name": "user Select Ey In The Ey Field",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 33,
+  "name": "user Enter The Cvv In The Ccv Field",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 34,
+  "name": "user Click The Book Now Button",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Stepdefinition1.enter_the_Options()"
+});
+formatter.result({
+  "duration": 69800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Stepdefinition1.user_Enter_The_Fname_In_The_Fname_Field()"
+});
+formatter.result({
+  "duration": 108394500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Stepdefinition1.user_Enter_The_Lname_In_The_Lname_Field()"
+});
+formatter.result({
+  "duration": 115765200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Stepdefinition1.user_Enter_The_Addres_In_The_Addres_Field()"
+});
+formatter.result({
+  "duration": 110137700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Stepdefinition1.user_Enter_The_Ccno_In_The_Ccno_Field()"
+});
+formatter.result({
+  "duration": 94082500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Stepdefinition1.user_Select_Cct_In_The_Cct_Field()"
+});
+formatter.result({
+  "duration": 110227000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Stepdefinition1.user_Select_Em_In_The_Em_Field()"
+});
+formatter.result({
+  "duration": 141668400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Stepdefinition1.user_Select_Ey_In_The_Ey_Field()"
+});
+formatter.result({
+  "duration": 145858400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Stepdefinition1.user_Enter_The_Cvv_In_The_Ccv_Field()"
+});
+formatter.result({
+  "duration": 125265800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Stepdefinition1.user_Click_The_Book_Now_Button()"
+});
+formatter.result({
+  "duration": 104200500,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 35,
+  "name": "Logout Page Functionality",
+  "description": "",
+  "id": "checking-the-hotel-booking-functionality-of-an-adactin-application;logout-page-functionality",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 36,
+  "name": "user Click The Logout Button And Exit The Appllication",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Stepdefinition1.user_Click_The_Logout_Button_And_Exit_The_Appllication()"
+});
+formatter.result({
+  "duration": 6084097300,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 37,
+  "name": "Logout Page Functionality",
+  "description": "",
+  "id": "checking-the-hotel-booking-functionality-of-an-adactin-application;logout-page-functionality",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 38,
+  "name": "to Tack Screen Shot",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Stepdefinition1.to_Tack_Screen_Shot()"
+});
+formatter.result({
+  "duration": 390683900,
+  "error_message": "java.io.IOException: Cannot create directory \u0027C:\\Users\\elang\\eclipse-workspace\\Maven_Pro\\ScreenShot\u0027.\r\n\tat org.apache.commons.io.FileUtils.mkdirs(FileUtils.java:2200)\r\n\tat org.apache.commons.io.FileUtils.createParentDirectories(FileUtils.java:1113)\r\n\tat org.apache.commons.io.FileUtils.copyFile(FileUtils.java:843)\r\n\tat org.apache.commons.io.FileUtils.copyFile(FileUtils.java:756)\r\n\tat com.Stepdefinition.Stepdefinition1.to_Tack_Screen_Shot(Stepdefinition1.java:191)\r\n\tat ✽.Then to Tack Screen Shot(D:/C drive/Yui/eclipse-workspace/cucumber/src/test/java/com/feature/Adactin.feature:38)\r\n",
+  "status": "failed"
+});
+});
--- target/test-classes/com/feature/google.feature
+++
@@ -1,4 +0,0 @@
-Feature: Google chrome launch
-Scenario: Google chrome launch and search
-Given User must launch the Browser
-When User Must Lauch the Url "https://www.google.com/" 
